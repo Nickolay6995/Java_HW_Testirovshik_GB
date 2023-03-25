a87cd5406c71c38479757a116e62cdb4c520a9a1
Nickolay Kochelaevskiy <nickolas-dragus@yandex.ru>
2023-03-25 22:06:56 +0300

    Add commit Tasks

---
+++ Seminar_1/src/main/java/Task1/MainClassFromTaskOne.java
@@ -0,0 +1,27 @@
+package Task1;
+
+//1. Задать одномерный массив и найти в нем минимальный и максимальный элементы
+
+public class MainClassFromTaskOne {
+
+    public static void main(String[] args) {
+        int[] mas = {7, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 5};
+        int min = mas[0], max = mas[0], i, imin = 0, imax = 0;
+        for(i = 0; i != mas.length; i++)
+        {
+            if(mas[i] < min){
+                min = mas[i];
+                imin = i;
+            }
+            if(mas[i] > max){
+                max = mas[i];
+                imax = i;
+            }
+            System.out.println("max = "+max+" min = "+min);
+            System.out.println("imax = "+imax+" imin = "+imin);
+        }
+    }
+}
+
+
+
---
+++ Seminar_1/src/main/java/Task3/MainClassFromTaskThree.java
@@ -0,0 +1,39 @@
+package Task3;
+
+//3*. В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида:
+//        "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+//        "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+//        "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+//        "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+//
+//public class MainClassFromTaskThree {
+//
+//import java.time.LocalDateTime;
+//import java.util.Calendar;
+//import java.util.GregorianCalendar;
+//import java.util.Scanner;
+//
+//
+//        public static void main(String[] args) {
+//
+//            // System.out.println("Привет, " + name);
+//            // if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) >= 5
+//            //         && Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 12) {
+//            //     System.out.println("Доброе утро, " + name + "!");
+//            // } else if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) >= 12
+//            //         && Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 18) {
+//            //     System.out.println("Добрый день, " + name + "!");
+//            // } else if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) >= 18
+//            //         && Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 23) {
+//            //     System.out.println("Добрый вечер, " + name + "!");
+//            // } else if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) >= 23
+//            //         && Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 5) {
+//            //     System.out.println("Доброй ночи, " + name + "!");
+//            // }
+//            Task myObject = new Task();
+//            myObject.maxOfOnes();
+//        }
+//    }
+//
+//
+//}
